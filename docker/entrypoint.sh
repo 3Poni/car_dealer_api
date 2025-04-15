1fa30184b1f7f2d991917a7cf4590c1b55f2f492
Valeriy Petrov <piswasser38@gmail.com>
2025-04-15 19:46:35 +0800

    laravel installed

---
+++ docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+fi
+
+php-fpm -D
+
+tail -f /dev/null
